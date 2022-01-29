4e4802a5349081b6bcb9e285757e53ab92a62ee2
thomaszub <thomas.zub@outlook.de>
2022-01-29 21:10:43 +0100

    Implemented game loop

--- rl-tic-tac-toe/game.py
+++ rl-tic-tac-toe/game.py
@@ -1,4 +1,7 @@
+from random import randint, randrange
+
 from domain.board import Board
+from domain.field import Field
 from domain.player import Player
 
 
@@ -16,4 +19,18 @@ class Game:
         self._board = Board()
 
     def start(self):
-        pass  # TODO
+        starting_player = randint(0, 1)
+        if starting_player == 0:
+            current_player = self._playerX
+        else:
+            current_player = self._playerO
+        player_won = None
+        while player_won == None:
+            player = Field.X if current_player == self._playerX else Field.O
+            action = current_player.take_turn(self._board)
+            self._board.set_field(action, player)
+            # TODO Determine win condition
+            current_player.board_changed(self._board, 0)
+            current_player = (
+                self._playerX if current_player == self._playerO else self._playerO
+            )
