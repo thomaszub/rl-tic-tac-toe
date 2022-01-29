c752455f821b77203971ed3762bf4b81547bcac8
thomaszub <thomas.zub@outlook.de>
2022-01-29 19:14:13 +0100

    Added human player

---
+++ rl-tic-tac-toe/human_player.py
@@ -0,0 +1,25 @@
+from domain.board import Board
+from domain.player import Player
+
+
+class HumanPlayer(Player):
+    _name: str
+
+    def __init__(self, name: str):
+        self._name = name
+
+    def take_turn(self, board: Board) -> tuple[int, int]:
+        print(board)
+        return self._get_user_input()
+
+    def _get_user_input(self) -> tuple[int, int]:
+        position = input(f"Player {self._name} choose a field (1-3, e.g. 3 1): ").split(
+            " "
+        )
+        if len(position) >= 2:
+            posX = position[0]
+            posY = position[1]
+            if posX.isdigit() and posY.isdigit():
+                return int(posX), int(posY)
+        print("Wrong input, try again!")
+        return self._get_user_input()
