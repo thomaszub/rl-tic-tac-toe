000d4be50f1a57d045437df0f4400acbf92c4e08
thomaszub <thomas.zub@outlook.de>
2022-01-29 17:21:26 +0100

    Added conversion toInt

--- rl-tic-tac-toe/domain/field.py
+++ rl-tic-tac-toe/domain/field.py
@@ -5,3 +5,11 @@ class Field(Enum):
     FREE = 1
     X = 2
     O = 3
+
+    def toInt(self, player: "Field") -> int:
+        if self == Field.FREE:
+            return 0
+        if self == player:
+            return 1
+        else:
+            return -1
