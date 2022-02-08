ce5985075c0ac863f5f226a5e5ef2cb0fcd4f9dc
thomaszub <thomas.zub@outlook.de>
2022-02-08 20:52:07 +0100

    Refactoring

--- rl-tic-tac-toe/game.py
+++ rl-tic-tac-toe/game.py
@@ -8,25 +8,29 @@ from domain.player import Player
 class Game:
     _board: Board
     _players: Tuple[Player, Player]
+    _current_player: Player
 
     def __init__(self, players: Tuple[Player, Player]) -> None:
-        self._board = Board()
         self._players = players
         if players[0].marker() == players[1].marker():
             raise ValueError("Players have the same marker")
+        self.reset()
 
     def reset(self) -> None:
         self._board = Board()
+        self._current_player = self._players[randint(0, 1)]
 
     def start(self) -> None:
-        current_player_num = randint(0, 1)
-        current_player = self._players[current_player_num]
         player_won = None
         while player_won == None:
-            action = current_player.take_turn(self._board)
-            self._board.mark_position(action, current_player.marker())
+            action = self._current_player.take_turn(self._board)
+            self._board.mark_position(action, self._current_player.marker())
             # TODO Determine win condition
-            current_player.board_changed(self._board, 0)
+            self._current_player.board_changed(self._board, 0)
+            self._current_player = self._next_player()
 
-            current_player_num = (current_player_num + 1) % 2
-            current_player = self._players[current_player_num]
+    def _next_player(self) -> Player:
+        if self._current_player == self._players[0]:
+            return self._players[1]
+        else:
+            return self._players[0]
