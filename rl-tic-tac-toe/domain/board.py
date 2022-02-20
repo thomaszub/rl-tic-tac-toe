28e15211de3e84f33b0f9d7e06ac574a60107885
thomaszub <thomas.zub@outlook.de>
2022-02-20 17:22:27 +0100

    Added win check

--- rl-tic-tac-toe/domain/board.py
+++ rl-tic-tac-toe/domain/board.py
@@ -1,3 +1,4 @@
+from telnetlib import GA
 from typing import List, Optional, Tuple
 
 from .gameresult import GameResult
@@ -19,8 +20,7 @@ class Board:
     ) -> Optional[GameResult]:
         self._validate_position(position)
         self._set_field(position, marker)
-        # TODO Check if won
-        return None
+        return self._get_gameresult(position, marker)
 
     def is_position_free(self, position: Tuple[int, int]) -> bool:
         if self._get_field(position) == None:
@@ -37,6 +37,42 @@ class Board:
 
     def _set_field(self, position: Tuple[int, int], marker: Marker) -> None:
         self._fields[position[0] - 1][position[1] - 1] = marker
+
+    def _get_gameresult(
+        self, position: Tuple[int, int], marker: Marker
+    ) -> Optional[GameResult]:
+        col = position[1] - 1
+        row = position[0] - 1
+        if (
+            self._fields[0][col] == marker
+            and self._fields[1][col] == marker
+            and self._fields[2][col] == marker
+        ):
+            return GameResult.Won
+        if (
+            self._fields[row][0] == marker
+            and self._fields[row][1] == marker
+            and self._fields[row][2] == marker
+        ):
+            return GameResult.Won
+        if (
+            self._fields[0][0] == marker
+            and self._fields[1][1] == marker
+            and self._fields[2][2] == marker
+        ):
+            return GameResult.Won
+        if (
+            self._fields[0][2] == marker
+            and self._fields[1][1] == marker
+            and self._fields[2][0] == marker
+        ):
+            return GameResult.Won
+        num_elems = len(
+            [field for col in self._fields for field in col if field != None]
+        )
+        if num_elems >= 9:
+            return GameResult.Draw
+        return None
 
     def __str__(self) -> str:
         col_strings = [
