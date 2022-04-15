10ca796e685fd613980ff5eee8dc3b304759571e
thomaszub <thomas.zub@outlook.de>
2022-04-15 14:51:42 +0200

    Base setup for Q learning agent

---
+++ rl-tic-tac-toe/agent_player.py
@@ -0,0 +1,58 @@
+from typing import Dict, List, Optional, Tuple
+
+from domain.board import Board
+from domain.gameresult import GameResult
+from domain.marker import Marker
+from domain.player import Player
+
+_State = List[List[Optional[Marker]]]
+_StateHash = int
+
+
+def _state_hash(state: _State) -> int:
+    flatten = tuple([field for col in state for field in col])
+    return hash(flatten)
+
+
+class QAgentPlayer(Player):
+    _state: _State
+    _action_values: Dict[_StateHash, Dict[Tuple[int, int], float]]
+
+    def __init__(self, marker: Marker) -> None:
+        super().__init__(marker)
+        self._state = []
+        self._action_values = {}
+
+    def take_turn(self, board: Board) -> Tuple[int, int]:
+        self._state = board.get_fields()
+        possible_actions = board.get_free_positions()
+        state_hash = _state_hash(self._state)
+        action_values_for_state = self._action_values.get(state_hash)
+        if action_values_for_state == None:
+            init_values = {action: 0.0 for action in possible_actions}
+            self._action_values[state_hash] = init_values
+            action_values_for_state = init_values
+
+        action = max(action_values_for_state, key=action_values_for_state.get)
+        self._action = action
+        return action
+
+    def board_changed(
+        self, new_board: Board, game_result: Optional[GameResult]
+    ) -> None:
+        pass
+
+    def save(self, file: str) -> None:
+        # TODO
+        pass
+
+    @staticmethod
+    def load(file: str) -> "QAgentPlayer":
+        # TODO
+        pass
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return f"Q-Agent {self.marker()}"
