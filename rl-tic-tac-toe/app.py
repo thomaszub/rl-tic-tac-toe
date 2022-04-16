1702dded147ac106e844e1e5ef6e7dec5a3dad84
thomaszub <thomas.zub@outlook.de>
2022-04-16 23:01:54 +0200

    Changed second player to agent

--- rl-tic-tac-toe/app.py
+++ rl-tic-tac-toe/app.py
@@ -1,12 +1,37 @@
-from agent_player import QAgentPlayer
+from copy import deepcopy
+
+from tqdm import trange
+
+from agent_player import QAgentPlayer, RandomAgentPlayer
 from domain.marker import Marker
 from game import Game
 from human_player import HumanPlayer
 
 
+def train(playerO: QAgentPlayer) -> None:
+    num_won = 0
+    num_lost = 0
+
+    playerX = RandomAgentPlayer(Marker.X)
+    playerO.training_mode(True)
+    with trange(1, 20000) as tr:
+        for _ in tr:
+            game = Game((playerX, playerO), False)
+            result = game.start()
+            if result == Marker.X:
+                num_lost += 1
+            elif result == Marker.O:
+                num_won += 1
+
+            tr.set_postfix(num_lost=num_lost, num_won=num_won)
+    playerO.training_mode(False)
+
+
 def main():
     playerX = HumanPlayer(Marker.X)
-    playerO = HumanPlayer(Marker.O)
+
+    playerO = QAgentPlayer(Marker.O, 0.1, 0.03, 1024, 32, 8)
+    train(playerO)
     game = Game((playerX, playerO))
     game.start()
 
